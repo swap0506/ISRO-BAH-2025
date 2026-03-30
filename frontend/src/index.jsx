011c8455e581d6d64f1e4aa5109137e1d8d93fe9
swap0506 <139144493+swap0506@users.noreply.github.com>
2026-03-31 00:58:31 +0530

    Fix JSX issue by renaming index.js to index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AirQuality from "./airquality";
+import AirQuality from "./airquality.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
