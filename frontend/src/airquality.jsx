d9aee73b187fc03121e782b67d0449bff34fb86c
swap0506 <139144493+swap0506@users.noreply.github.com>
2026-03-31 02:04:44 +0530

    Connected frontend to Render backend

--- frontend/src/airquality.jsx
+++ frontend/src/airquality.jsx
@@ -853,7 +853,7 @@ function PredictionPage() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://isro-bah-2025-5.onrender.com/predict"), {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
